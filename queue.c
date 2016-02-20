cb06590e5e5cbbe13cafb28c0870eeb00fae16c2
Shantanu Chhabra <shantanuchhabra@Shantanus-MBP.wv.cc.cmu.edu>
2016-02-20 15:35:30 -0500

    added some more code and changed the structs a little

--- queue.c
+++ queue.c
@@ -3,6 +3,7 @@ typedef void* elem;
 typedef struct queue_node {
     elem data;
     struct queue_node* next;
+    struct queue_node* prev;
 } queue_node
 
 typedef struct queue {
@@ -20,6 +21,7 @@ queue_t queue_empty() {
     queue_node* dummy = malloc(sizeof(queue_node));
     dummy->data = NULL;
     dummy->next = NULL;
+    dummy->prev = NULL;
     Q->front = malloc(sizeof(queue_node));
     Q->back = malloc(sizeof(queue_node));
     Q->front = dummy;
@@ -31,13 +33,22 @@ queue_t queue_empty() {
 void enq (queue_t Q, elem data) {
     queue_node* new_elem = malloc(sizeof(queue_node));
     new_elem->data = data;
-    new_elem->next = NULL;
-    Q->back->next = new_elem;
+    if (queue_empty(Q)) {
+        // queue is empty
+        new_elem->prev = NULL;
+        new_elem->next = Q->back;
+        Q->back->prev = new_elem;
+        Q->front = new_elem;
+    }
+    else {
+        // queue is not empty
+        new_elem->next = Q->back;
+    }
 }
 
 queue_node* deq(queue_t Q) {
     if (Q->size == 0) return NULL;
-    queue_node* 
+    queue_node* elem = Q->
 }
 
 
