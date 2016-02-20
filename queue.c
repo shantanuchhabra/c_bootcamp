281fc730b902b249f0dc19c789be77111d164cb9
Shantanu Chhabra <shantanuchhabra@Shantanus-MBP.wv.cc.cmu.edu>
2016-02-20 16:56:33 -0500

    Queue stuff implemented and working

--- queue.c
+++ queue.c
@@ -1,29 +1,36 @@
-typedef void* elem;
+#include <stdlib.h>
+#include <stdio.h>
+#include "queue.h"
 
-typedef struct queue_node {
+struct queue_node {
     elem data;
     struct queue_node* next;
     struct queue_node* prev;
-} queue_node
+};
 
-typedef struct queue {
+struct queue {
     queue_node* front; // Points to root queue_node
     queue_node* back;
     int size;
-} queue
+};
 
-bool queue_empty (queue_t Q) {
+bool is_queue_empty (queue_t Q) {
+    if (Q == NULL) {
+        fprintf(stderr, "NULL Queue.");
+        exit(1);
+    }
+    if (Q->size == 0) {
+        printf("Empty queue\n");
+    }
     return (Q->size == 0);
 }
 
-queue_t queue_empty() {
+queue_t queue_new() {
     queue_t Q = malloc(sizeof(queue));
     queue_node* dummy = malloc(sizeof(queue_node));
     dummy->data = NULL;
     dummy->next = NULL;
     dummy->prev = NULL;
-    Q->front = malloc(sizeof(queue_node));
-    Q->back = malloc(sizeof(queue_node));
     Q->front = dummy;
     Q->back = Q->front;
     Q->size = 0;
@@ -31,26 +38,91 @@ queue_t queue_empty() {
 }
 
 void enq (queue_t Q, elem data) {
-    queue_node* new_elem = malloc(sizeof(queue_node));
-    new_elem->data = data;
-    if (queue_empty(Q)) {
+    queue_node* new_node = malloc(sizeof(queue_node));
+    new_node->data = data;
+    if (is_queue_empty(Q)) {
         // queue is empty
-        new_elem->prev = NULL;
-        new_elem->next = Q->back;
-        Q->back->prev = new_elem;
-        Q->front = new_elem;
+        new_node->prev = NULL;
+        new_node->next = Q->back;
+        Q->back->prev = new_node;
+        Q->front = new_node;
     }
     else {
         // queue is not empty
-        new_elem->next = Q->back;
+        new_node->next = Q->back;
+        new_node->prev = Q->back->prev;
+        Q->back->prev = new_node;
+        new_node->prev->next = new_node;
     }
+    Q->size += 1;
 }
 
 queue_node* deq(queue_t Q) {
-    if (Q->size == 0) return NULL;
-    queue_node* elem = Q->
+    if (is_queue_empty(Q)) {
+        return NULL;
+    }
+    queue_node* node = Q->front;
+    Q->front->next->prev = NULL;
+    Q->front = Q->front->next;
+    Q->size -= 1;
+    return node;
 }
 
+void free_queue_node(queue_node* node) {
+    if (node == NULL) return;
+    free(node->data);
+    free_queue_node(node->next);
+    free(node);
+}
 
+void free_queue(queue_t Q) {
+    free_queue_node(Q->front);
+    free(Q);
+}
+
+void print_node_data(elem data) {
+    printf("%d ", *data);
+}
+
+void print_queue(queue_t Q) {
+    queue_node* p = Q->front;
+    while (p != Q->back) {
+        print_node_data(p->data);
+        p = p->next;
+    }
+}
+
+int main() {
+    queue_t Q = queue_new();
+    int* data1 = malloc(sizeof(int)); 
+    *data1 = 1;
+    int* data2 = malloc(sizeof(int)); 
+    *data2 = 2;
+    int* data3 = malloc(sizeof(int)); 
+    *data3 = 3;
+    int* data4 = malloc(sizeof(int)); 
+    *data4 = 42;
+    enq(Q, data1);
+    enq(Q, data2);
+    enq(Q, data3);
+    enq(Q, data4);
+    print_queue(Q);
+    printf("\n");
+    deq(Q);
+    print_queue(Q);
+    printf("\n");
+    deq(Q);
+    print_queue(Q);
+    printf("\n");
+    deq(Q);
+    print_queue(Q);
+    printf("\n");
+    deq(Q);
+    print_queue(Q);
+    printf("\n");
+    deq(Q);
+    print_queue(Q);
+    return 0;
+}
 
 
--- queue.h
+++ queue.h
@@ -1,12 +1,21 @@
-/*** Interface ***/
+#ifndef QUEUE_H
+#define QUEUE_H
 
+#include <stdbool.h>
+
+typedef int* elem;
 typedef struct queue_node queue_node;
-typedef struct queue_header* queue_t;
+typedef struct queue queue;
+typedef struct queue* queue_t;
 
-bool queue_empty(queue_t Q)
+bool is_queue_empty(queue_t Q);
 
-queue_t queue_new()
+queue_t queue_new();
 
-void enq(queue_t Q, string e)
+void enq(queue_t Q, elem e);
 
-queue_node deq(queue_t Q)
+queue_node* deq(queue_t Q);
+void free_queue_node(queue_node* node);
+void free_queue(queue_t Q);
+
+#endif
