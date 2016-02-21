99bea9486f3631d752fa24d7b72d81e01b4407ac
Sol Boucher <sboucher@cmu.edu>
2016-02-21 02:52:17 -0500

    Move main() function to a separate module
    I think this makes a better demo because the queue stuff is now encapsulated and
    reusable from other compilation units and even whole programs.

---
+++ demo.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "queue.h"
+
+int main(void) {
+    queue_t Q = queue_new();
+    int* data1 = malloc(sizeof(int));
+    *data1 = 1;
+    int* data2 = malloc(sizeof(int));
+    *data2 = 2;
+    int* data3 = malloc(sizeof(int));
+    *data3 = 3;
+    int* data4 = malloc(sizeof(int));
+    *data4 = 42;
+    enq(Q, data1);
+    enq(Q, data2);
+    enq(Q, data3);
+    enq(Q, data4);
+    print_queue(Q);
+    // free_queue(Q); // Make this function call when you want to demo free
+    deq(Q);
+    print_queue(Q);
+    deq(Q);
+    print_queue(Q);
+    deq(Q);
+    print_queue(Q);
+    deq(Q);
+    print_queue(Q);
+    deq(Q);
+    print_queue(Q);
+    return 0;
+}
--- queue.c
+++ queue.c
@@ -93,34 +93,3 @@ void print_queue(queue_t Q) {
     }
     putchar('\n');
 }
-
-int main(void) {
-    queue_t Q = queue_new();
-    int* data1 = malloc(sizeof(int)); 
-    *data1 = 1;
-    int* data2 = malloc(sizeof(int)); 
-    *data2 = 2;
-    int* data3 = malloc(sizeof(int)); 
-    *data3 = 3;
-    int* data4 = malloc(sizeof(int)); 
-    *data4 = 42;
-    enq(Q, data1);
-    enq(Q, data2);
-    enq(Q, data3);
-    enq(Q, data4);
-    print_queue(Q);
-    // free_queue(Q); // Make this function call when you want to demo free
-    deq(Q);
-    print_queue(Q);
-    deq(Q);
-    print_queue(Q);
-    deq(Q);
-    print_queue(Q);
-    deq(Q);
-    print_queue(Q);
-    deq(Q);
-    print_queue(Q);
-    return 0;
-}
-
-
